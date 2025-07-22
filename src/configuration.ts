0b959111ce0ab4085972fed8bd83afa2dddbf872
mzy <671298798@qq.com>
2025-07-22 19:51:02 +0800

    login

--- src/configuration.ts
+++ src/configuration.ts
@@ -4,6 +4,7 @@ import * as validate from '@midwayjs/validate';
 import * as info from '@midwayjs/info';
 import * as swagger from '@midwayjs/swagger';
 import * as orm from '@midwayjs/typeorm';
+import * as cors from '@koa/cors';
 import { join } from 'path';
 // import { DefaultErrorFilter } from './filter/default.filter';
 // import { NotFoundFilter } from './filter/notfound.filter';
@@ -31,5 +32,6 @@ export class MainConfiguration {
     this.app.useMiddleware([ReportMiddleware]);
     // add filter
     // this.app.useFilter([NotFoundFilter, DefaultErrorFilter]);
+    this.app.use(cors()); // 允许所有跨域
   }
 }
